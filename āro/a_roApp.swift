cd4539166b8a43b5ef2834d0e2c97dbbd42fe720
Zachary <zjhzingo@icloud.com>
2022-01-20 16:24:57 -0500

    Initial Commit

---
+++ āro/a_roApp.swift
@@ -0,0 +1,17 @@
+//
+//  a_roApp.swift
+//  āro
+//
+//  Created by Zachary Hixon on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct a_roApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ āroUITests/a_roUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  a_roUITestsLaunchTests.swift
+//  āroUITests
+//
+//  Created by Zachary Hixon on 1/20/22.
+//
+
+import XCTest
+
+class a_roUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
